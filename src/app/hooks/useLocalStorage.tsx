d915129dd391c403b4279e4e9a0627bde03f84b5
Erick Alen <erick.alen@live.com>
2024-03-31 20:37:01 -0300

    feat: adding 'useLocalStorage' hook

---
+++ src/app/hooks/useLocalStorage.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+
+type useLocalStorageProps = {
+  key: string;
+  init: string;
+};
+
+export const useLocalStorage = ({ key, init }: useLocalStorageProps) => {
+  const [localStorageKey, setLocalStorageKey] = useState<string>(() => {
+    const localStorageValue = localStorage.getItem(key);
+    return localStorageValue ? localStorageValue : init;
+  });
+  useEffect(() => {
+    localStorage.setItem(key, localStorageKey);
+  });
+  
+  // 'as const' defines that the order of this array will never change
+  return [localStorageKey, setLocalStorageKey] as const;
+};
